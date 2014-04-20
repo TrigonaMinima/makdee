3b8a6633188fb21a329f4a6b486bd96753e4478d
Sharan Agrawal <sharan.jmi@gmail.com>
2014-04-21 03:10:12 +0530

    Update Sender2.c

--- Sender2.c
+++ Sender2.c
@@ -1,15 +1,15 @@
 # include <stdio.h>
 # include <stdlib.h>
-# include <sys/msg.h>
 
 # include "common.h"
+# include <sys/msg.h>
 
 int main()
 {
         int msgid, i=0, j=0, size;
         char ch;
         struct mcd order;
-        order.identity = 75;
+        order.identity = 25;
         size = sizeof(struct mcd) - sizeof(long int);
 
         // setting up the msg queue
@@ -22,8 +22,8 @@ int main()
 
         while(1)
         {
-                system("clear");
                 ch='y';
+                order.count=0;
                 order.my_msg_type = 1;
                 zero(order.dish_no);
                 zero(order.quant);
@@ -31,14 +31,23 @@ int main()
                 while(ch == 'y' || ch == 'Y')
                 {
                         printf("\nPlease enter the serial number of the dish you want to order : ");
-                        scanf("%d", &(order.dish_no[i]));
+                        scanf("%d", &(order.dish_no[order.count]));
                         printf("Please enter the quantity of the dish you want to order : ");
-                        scanf("%d", &(order.quant[i]));
-                        i++;
+                        scanf("%d", &(order.quant[order.count]));
+                        order.count++;
                         printf("Do you want to order more ? (y/n) : ");
                         scanf("%s", &ch);
                 }
                 //receipt(order.dish_no, order.quant, order.id);
+                j=0;
+                /*
+                while(order.quant[j]>0)
+                {
+                	printf("\n%d\n",order.dish_no[j]);
+                	printf("%d\n",order.quant[j]);
+                	j++;
+                }
+                //sleep(10);*/
 
                 if(msgsnd(msgid, (void *)&order, size, 0) == -1)
                 {
@@ -49,6 +58,7 @@ int main()
                 order.identity += 1;
                 if(order.dish_no[0] <= 0)
                         break;
+                system("clear");
         }
         return 0;
 }
